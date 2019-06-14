0e5f97a73613fa3a7597edf70c4679d85a6432a2
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-06-14 16:11:52 +0200

    Changes for Linux

--- main.c
+++ main.c
@@ -15,11 +15,13 @@ int main()
                 return 63;
         }
 
+#ifdef __APPLE__
         glfwWindowHint(GLFW_SAMPLES, 4);               // 4x antialiasing
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // To make MacOS happy; should not be needed
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+#endif
 
         GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
         if (!window) {
